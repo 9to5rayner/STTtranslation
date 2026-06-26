a6550d4144278cc6fe5f1c1841e02436e3ea4a56
9to5rayner <9to5rayner@gmail.com>
2026-06-26 14:42:21 +0700

    "Added app files and build script"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,23 @@
 // Root build.gradle.kts
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Tells the system how to read Android compile tasks
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
