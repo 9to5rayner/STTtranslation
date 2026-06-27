0b07636895dc1284d9e2618d6ccfa02a1a84b296
9to5rayner <9to5rayner@gmail.com>
2026-06-27 17:34:52 +0700

    "Added app files and build script"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.groqtranscriber"
-        minSdk = 26 // Supports Android 8.0 and above
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -35,11 +35,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
     // Networking & JSON
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
-    
+
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
